55c8655cab6a6652976dee10b5b895296dfe8a9e
Scythark <143963537+Scythark@users.noreply.github.com>
2024-02-25 01:05:56 -0600

    Add files via upload
    Face Detection

---
+++ import cv2.py
@@ -0,0 +1,41 @@
+import cv2
+import datetime 
+import time 
+cap = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+body_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_fullbody_default.xml")
+
+
+
+
+
+
+while True:
+    x, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    
+    for (x, y, width, height) in faces:
+        cv2.rectangle(frame, (x, y), (x + width, y + height), (255, 0, 0), 3)
+    
+    cv2.imshow("camera", frame)
+    
+    
+    if cv2.waitKey(1) == ord('q'):
+        
+        
+        break
+
+
+
+
+
+
+
+cap.release()
+cv2.destroyALLWindows()    
+
